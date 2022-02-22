fd70c9f514e274e8a81d625853b3698017870dfa
EderReisS <ederdt.3@gmail.com>
2022-02-21 23:50:16 -0300

    commit

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento')
+    list_filter = ('usuario','data_evento')
+
 
 # Register your models here.
+admin.site.register(Evento,EventoAdmin)
